f02be9b2e7d8f73a37ad0ad7539181d10cf12b18
Sandeep Nigam <sandeepnigam379@gmail.com>
2022-10-14 22:54:43 +0530

    Time: 12 ms (68.06%), Space: 11.1 MB (59.80%) - LeetHub

---
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.cpp
@@ -0,0 +1,59 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    
+    ListNode* fixHead(ListNode* head){
+        while(head->next!=NULL && head->val == head->next->val){
+            head=head->next;
+        }
+        if(head==NULL || head->next==NULL){
+            return NULL;
+        }
+        head=head->next;
+        return head;
+    }
+    
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(head==NULL || head->next==NULL){
+            return head;
+        }
+        
+        while(head!=NULL && head->next!=NULL && head->val==head->next->val){
+            head=fixHead(head);
+        }
+        if(head==NULL || head->next==NULL){
+            return head;
+        }
+        
+        ListNode* before=head;
+        ListNode* after=head->next;
+        while(after->next!=NULL){
+            if(after->val == after->next->val){
+                while(after->next!=NULL && after->val==after->next->val){
+                    after=after->next;
+                }
+                if(after->next==NULL){
+                    before->next=NULL;
+                }
+                else{
+                    after=after->next;
+                    before->next=after;
+                }
+            }
+            else{
+                before=after;
+                after=after->next;
+            }
+        }
+        return head;
+    }
+};
