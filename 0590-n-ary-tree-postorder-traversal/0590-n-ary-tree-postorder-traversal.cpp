df9f92b8c1f0d5e8b010b37abb88035cbbfddc9a
Sandeep Nigam <sandeepnigam379@gmail.com>
2022-10-18 15:06:14 +0530

    Time: 545 ms (5.17%), Space: 134.4 MB (5.11%) - LeetHub

---
+++ 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.cpp
@@ -0,0 +1,51 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    stack<int> helper(Node* root, stack<int> s) {
+        // cout<<"size is:"<<root->children.size()<<endl;
+        if(root->children.size()==0){
+            s.push(root->val);    
+            return s;
+        }
+        // cout<<"inserting-->"<<root->val<<endl;
+        s.push(root->val);
+        for(int i=root->children.size()-1;i>=0;i--){
+            s=helper(root->children[i], s);
+        }
+        return s;
+    }
+    
+    vector<int> postorder(Node* root) {
+        vector<int> ans;
+        if(root==NULL){
+            return ans;
+        }
+        stack<int> hold;
+        hold = helper(root,hold);
+        while(hold.size()!=0){
+            ans.push_back(hold.top());
+            hold.pop();
+        }
+        
+        return ans;
+    }
+};
